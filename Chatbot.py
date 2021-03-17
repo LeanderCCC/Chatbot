df3b94ac7cadd55e39ea96af988f93021f59fd25
LeanderCCC <79199855+LeanderCCC@users.noreply.github.com>
2021-03-17 12:15:24 +0100

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -24,6 +24,7 @@ class MyClient(discord.Client):
                 Hallo = ["Hi", "Hallo", "Moin", "Hi was geht?", "Servus", "Guten Tag", "Ich Grüße sie"]
                 Hallo = random.choice(Hallo)
                 time.sleep(1.5)
+                message.author = message.author[-6:]
                 await message.channel.send(Hallo + " " + str(message.author))
         elif message.content.startswith("HELLO"):
             time.sleep(1.5)
